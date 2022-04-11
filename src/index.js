c7940640700c1bfaf24baf81962d3767f57f6db5
Chandresh98 <99880909+Chandresh98@users.noreply.github.com>
2022-04-11 12:07:55 +0530

    start

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const mongoose = require('mongoose')
+const router = require('./router/router')
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect('mongodb+srv://arnabbiswas_14:arnabbiswas@cluster0.b95gv.mongodb.net/group-11-Database?retryWrites=true&w=majority',{useNewUrlParser:true})
+.then( () =>console.log("mongoose is contected..."))
+.catch( err => console.log(err))
+
+app.use('/', router)
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log(" Express App Running on port " +  (process.env.PORT || 3000));
+});
